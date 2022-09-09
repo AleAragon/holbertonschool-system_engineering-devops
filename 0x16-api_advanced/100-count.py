d97d7adcecd065bf2ed5113833cda0f5d845bffa
Alejo Aragon <alejo.aragon.05@gmail.com>
2022-09-09 10:42:21 -0500

    update_project

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,53 +1,54 @@
 #!/usr/bin/python3
-"""
-Module for storing the count_words function.
-"""
+"""Function to count words in all hot posts of a given Reddit subreddit."""
 import requests
 
-get = requests.get
-url = 'https://www.reddit.com/r/{}/hot.json'
-user_agent_str = ('Mozilla/5.0 (Windows NT 6.1; WOW64; rv:77.0)'
-                  'Gecko/20190101 Firefox/77.0')
-user_ag = {'User-Agent': user_agent_str}
-payload_default = {'limit': 100}
 
-
-def count_words(subreddit, word_list, payload={}, hot_list=[]):
+def count_words(subreddit, word_list, instances={}, after="", count=0):
+    """Prints counts of given words found in hot posts of a given subreddit.
+    Args:
+        subreddit (str): The subreddit to search.
+        word_list (list): The list of words to search for in post titles.
+        instances (obj): Key/value pairs of words/counts.
+        after (str): The parameter for the next page of the API results.
+        count (int): The parameter of results matched thus far.
     """
-    Prints the count of the given words present in the title of the
-    subreddit's hottest articles.
-    """
-    url = url.format(subreddit)
-
-    if payload == {}:
-        payload = payload_default
-
-    request = get(url, headers=user_ag)
-
-    if (request.status_code == 404):
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    try:
+        results = response.json()
+        if response.status_code == 404:
+            raise Exception
+    except Exception:
+        print("")
         return
 
-    request = get(url, headers=user_ag, params=payload)
-    request_data = request.json().get('data')
-
-    post_page = request_data.get('children')
-    hot_list.extend([post.get('data').get('title') for post in post_page])
-
-    if (request_data.get('after') is not None):
-        payload = {
-            'after': request_data.get('after'),
-            'limit': 100
-        }
-        return count_words(subreddit, word_list, payload, hot_list)
-
-    word_list = [word.lower() for word in word_list]
-    freq_dict = {word: 0 for word in word_list}
-    for title in hot_list:
-        title_words = title.lower().split(' ')
+    results = results.get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        title = c.get("data").get("title").lower().split()
         for word in word_list:
-            freq_dict[word] += title_words.count(word)
+            if word.lower() in title:
+                times = len([t for t in title if t == word.lower()])
+                if instances.get(word) is None:
+                    instances[word] = times
+                else:
+                    instances[word] += times
 
-    freq_list = [(key, val) for key, val in freq_dict.items() if val != 0]
-
-    for entry in sorted(freq_list, key=lambda x: (-x[1], x[0])):
-        print("{}: {}".format(entry[0], entry[1]))
+    if after is None:
+        if len(instances) == 0:
+            print("")
+            return
+        instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
+        [print("{}: {}".format(k, v)) for k, v in instances]
+    else:
+        count_words(subreddit, word_list, instances, after, count)
